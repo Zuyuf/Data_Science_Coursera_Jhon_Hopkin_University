f7129ac6e2c6c095da94233cfc4be6e04f53f9ab
zuyufullah manna <zuyufmanna@gmail.com>
2020-10-13 15:29:46 +0530

    3-G&C - WEEK1 - swril exercises

---
+++ Data Science Foundations using R/3 - Getting and Cleaning Data/WEEK 1 - Intro, Reading Various Files/02_practical-r-exercises-in-swirl/1 - Practical R Exercises in swirl Part 1.R
@@ -0,0 +1,30 @@
+###   1. Install swirl   ###
+# Since swirl is an R package, you can easily install it by entering a single command from the R console:
+# install.packages("swirl")
+
+
+####  If you've installed swirl in the past make sure you have version 2.2.21 or later. You can check this with:
+packageVersion("swirl")
+
+
+
+###    2. Load swirl   ###
+# Every time you want to use swirl, you need to first load the package. From the R console:
+library(swirl)
+
+
+###  3. Install the Getting and Cleaning Data course  ###
+#swirl offers a variety of interactive courses, but for our purposes, you want the one called Getting and Cleaning Data
+# Type the following from the R prompt to install this course:
+install_from_swirl("Getting and Cleaning Data")
+
+
+
+
+###   4. Start swirl and complete the lessons   #
+# Type the following from the R console to start swirl:
+swirl()
+
+
+
+# Then, follow the menus and select the Getting and Cleaning Data course when given the option.
